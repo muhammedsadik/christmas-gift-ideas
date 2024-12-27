7aaf10469e88bbb6a42bd636a12178a7308ea2dc
Sadık Orak <muhammedsadikorak@gmail.com>
2024-12-28 02:00:36 +0300

    Christmas gift idea app finished.

---
+++ assets/js/gift-idea.js
@@ -0,0 +1,79 @@
+const giftIdeaMsg = "İptal: Exist\n\nEnter your christmas gift idea:"
+const invalidEntry = "Invalid Entry, Try again.";
+const continueMsg = "İptal: Exist\n\nDo you want to continue: (Y/N)"
+const exited = "You exited from app.";
+const options = ["y", "n"];
+
+let ideas = [];
+
+if (localStorage.ideas) {
+  ideas = JSON.parse(localStorage.ideas);
+}
+
+function giftIdea() {
+  let value = prompt(giftIdeaMsg);
+
+  if (value === null) {
+    return false;
+  }
+
+  value = value.trim();
+
+  if (!value) {
+    alert(invalidEntry);
+    return giftIdea();
+  }
+
+  ideas.push( value );
+
+  localStorage.ideas = JSON.stringify(ideas);
+
+  if (!isContinue()) {
+    return false;
+  }
+}
+
+function isContinue() {
+  let value = prompt(continueMsg);
+
+  if (value === null) {
+    return false;
+  }
+
+  value = value.trim();
+
+  if (!value) {
+    alert(invalidEntry);
+    return isContinue();
+  }
+
+  value = value.toLowerCase();
+
+  if (!options.includes(value)) {
+    return isContinue();
+  }
+
+  if (value === "n") {
+    return false;
+  }
+
+  return giftIdea();
+}
+
+
+function mainMenu() {
+
+  if (ideas.length > 0) {
+    let ideasAlert = ideas.map((i, index) => `${index + 1} - ${i}`).join("\n");
+    alert(ideasAlert);
+  }
+
+  let result = giftIdea();
+
+  if (result === false) {
+    alert(exited);
+    return;
+  }
+}
+
+mainMenu();
